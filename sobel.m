9701dfc0700f2caee734cf9628c4ff9547ec9c76
Kaavya <94700095+kaavyananna@users.noreply.github.com>
2022-09-26 02:19:51 -0700

    Update sobel.m

--- sobel.m
+++ sobel.m
@@ -7,7 +7,7 @@ figure;
 imshow(inputImage); 
 title('Original Image');
 
-% converting color image to gray 
+% converting color image to gray scale image
 A = rgb2gray(inputImage); 
 figure;
 imshow(A);
@@ -20,7 +20,7 @@ for i=1: size(B,1)-2
     for j=1: size(B,2)-2 
         Gx = ((2*B(i+2,j+1)+B(i+2,j)+B(i+2,j+2))-(2*B(i,j+1)+B(i,j)+B(i,j+2))); 
         Gy = ((2*B(i+1,j+2)+B(i,j+2)+B(i+2,j+2))-(2*B(i+1,j)+B(i,j)+B(i+2,j))); 
-        A(i,j) = sqrt(Gx.^2+Gy.^2); 
+        A(i,j) = sqrt(Gx.^2+Gy.^2);
     end 
 end 
 
